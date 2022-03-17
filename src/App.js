6029ee5b42c67a004fdde8c88145aa0311da10a3
chukwuma Benjamin <doochukbeni@gmail.com>
2022-03-17 21:11:43 +0400

    added gitignore

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import LayoutRoute from "./LayoutRout.js";
+import LayoutRoute from "./LayoutRoute.js";
 import MainScreen from './MainScreen.js';
 import AboutScreen from "./AboutScreen.js";
 import PricingScreen from "./PricingScreen.js";
 import ProfileSettingsScreen from "./ProfileSettingsScreen.js";
 import LoginScreen from "./LoginScreen.js";
+import RegistrationScreen from "./RegistrationScreen.js";
 
 function App() {
     return (
         <BrowserRouter>
             <Switch>
                 <LayoutRoute path="/" component={MainScreen} exact={true} />
-                <LayoutRoute path="/pricing" component={PricingScreen} exact={true} />
                 <LayoutRoute path="/about" component={AboutScreen} exact={true} />
+                <LayoutRoute path="/pricing" component={PricingScreen} exact={true} />
                 <LayoutRoute path="/profile-settings" component={ProfileSettingsScreen} exact={true} />
-                <LayoutRoute path="/Login" component={LoginScreen} exact={true} />
-                
+                <LayoutRoute path="/login" component={LoginScreen} exact={true} />
+                <LayoutRoute path="/register" component={RegistrationScreen} exact={true} />
             </Switch>
         </BrowserRouter>
     )
