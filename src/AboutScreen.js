e4c0d321b774fbe27d6ccacff0579fb669c6d5a5
chukwuma Benjamin <doochukbeni@gmail.com>
2022-03-17 20:41:17 +0400

    Deploying to Heroku

---
+++ src/AboutScreen.js
@@ -0,0 +1,15 @@
+import NavBar from "./NavBar"
+
+function AboutScreen(props) {
+
+    return (
+        <div>
+
+            
+            <h1>About Screen</h1>
+
+        </div>
+    )
+};
+
+export default AboutScreen;
---
+++ src/MainScreen.js
@@ -0,0 +1,17 @@
+import NavBar from "./NavBar";
+import Hero from "./Hero";
+import Features from "./Features";
+import Footer from "./Footer";
+
+function MainScreen(props) {
+  return (
+    <div>
+      
+      <Hero></Hero>
+      <Features></Features>
+      
+    </div>
+  );
+}
+
+export default MainScreen;
